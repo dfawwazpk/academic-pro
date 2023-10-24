53fe18979a15a79b7d31ec634a23b0324762f1a6
Dimas <dfawwazpk@outlook.com>
2023-10-24 17:37:57 +0700

    fixed navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,25 +1,25 @@
-<nav class="navbar navbar-expand-lg bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand text-white" href="/">AcademicPro</a>
+        <a class="navbar-brand gittext-white" href="/">AcademicPro</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link text-white {{ ($title === "Dashboard") ? 'active' : '' }}" href="/">Dashboard</a>
+                    <a class="nav-link {{ ($title === "Dashboard") ? 'active' : '' }}" href="/">Dashboard</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white {{ ($title === "Daftar Mahasiswa") ? 'active' : '' }}" href="/">Daftar Mahasiswa</a>
+                    <a class="nav-link {{ ($title === "Daftar Mahasiswa") ? 'active' : '' }}" href="/">Daftar Mahasiswa</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white {{ ($title === "Daftar Dosen") ? 'active' : '' }}" href="/">Daftar Dosen</a>
+                    <a class="nav-link {{ ($title === "Daftar Dosen") ? 'active' : '' }}" href="/">Daftar Dosen</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white {{ ($title === "Buat Akun Mahasiswa") ? 'active' : '' }}" href="/">Buat Akun Mahasiswa</a>
+                    <a class="nav-link {{ ($title === "Buat Akun Mahasiswa") ? 'active' : '' }}" href="/">Buat Akun Mahasiswa</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white {{ ($title === "Buat Akun Dosen") ? 'active' : '' }}" href="/">Buat Akun Dosen</a>
+                    <a class="nav-link {{ ($title === "Buat Akun Dosen") ? 'active' : '' }}" href="/">Buat Akun Dosen</a>
                 </li>
             </ul>
         </div>
