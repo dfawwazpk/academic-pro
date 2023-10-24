848704e7d40a57cf4943cf920d9bddda9130f5d3
Dimas <dfawwazpk@outlook.com>
2023-10-24 12:04:09 +0700

    using sidebar instead of navbar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,9 +7,17 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     </head>
     <body>
-        @include("partials.navbar")
-        <div class="container mt-4">
-            @yield('container')
+        <div class="page-container">
+            <div class="content-wrap">
+                <div class="row">
+                    <div class="col-3 col-lg-2">
+                        @include("partials.sidebar")
+                    </div> 
+                    <div class="col-9 col-lg-10">
+                        @yield('container')
+                    </div> 
+                </div>
+            </div>
         </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     </body>
