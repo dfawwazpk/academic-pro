0b8b1b65943caa0ed201a21591c2ec46f62ed2c3
Dimas <dfawwazpk@outlook.com>
2023-10-24 10:21:06 +0700

    add bootstrap and blade layout

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/laravel', function () {
+    return view('laravel_welcome');
 });
