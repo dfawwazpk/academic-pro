875bd818e0fbe26161f8e90258bfe99b31f60718
DzakiP <dzakiprasetyo833@gmail.com>
2023-11-06 17:07:25 +0700

    login baru hehe

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,39 +1,45 @@
-@extends('layouts.main')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.15/tailwind.min.css">
+<section class="flex flex-col md:flex-row h-screen items-center">
 
-@section('container')
-<div class="row justify-content-center">
-    <div class="col-md-3 d-flex justify-content-center align-items-center">
-        <main class="form-signin w-100 m-auto">
-            <h1 class="mb-3 fw-bold text-center">AcademicPro</h1>
-            @if (session()->has('success'))
+    <div class="bg-black hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
+      <img src="https://if.fsm.undip.ac.id/id//assets/img/slider_home/slide1.png" alt="" class="w-full h-full object-cover">
+    </div>
+  
+    <div class="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
+          flex items-center justify-center">
+  
+      <div class="w-full h-100">
+        <h1 class="text-xl md:text-2xl font-bold leading-tight mt-12 text-center">AcademicPro</h1>
+        @if (session()->has('success'))
             <div class="alert alert-success alert-dismissible fade show" role="alert">
                 {{ session('success') }}
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
-            @endif
-            @if (session()->has('loginError'))
+        @endif
+        @if (session()->has('loginError'))
             <div class="alert alert-danger alert-dismissible fade show" role="alert">
                 {{ session('loginError') }}
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
-            @endif
-            <form action="/login" method="post">
-                @csrf
-                <div class="form-floating">
-                    <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" autofocus value="{{ old('email') }}">
-                    <label for="email">Email</label>
-                    @error('email')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="form-floating">
-                    <input type="password" name="password" class="form-control" id="password" placeholder="Password">
-                    <label for="password">Password</label>
-                </div>
-                <button class="btn btn-dark w-100 py-2" type="submit">Login</button>
-            </form>
-        </main>
-    </div>
-</div>
+        @endif
+        <form action="/login" class="mt-6"  method="POST">
+            @csrf
+            <div>
+            <label class="block text-gray-700">Email</label>
+            <input type="email" name="email" class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" @error('email') is-invalid @enderror" id="email" placeholder="Masukan Email Anda" @error('email') is-invalid @enderror" autofocus value="{{ old('email') }}" autocomplete required>
+          </div>
+  
+          <div class="mt-4">
+            <label class="block text-gray-700">Password</label>
+            <input type="password" name="password" class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 id="password" placeholder="Enter Password" minlength="6" 
+                  focus:bg-white focus:outline-none" required>
+          </div>
+          <button type="submit" class="w-full block bg-black hover:bg-zinc-400 focus:bg-zinc-400 text-white font-semibold rounded-lg
+                px-4 py-3 mt-6">Log In</button>
+        </form>
+  
+        <hr class="my-6 border-gray-300 w-full">
 
-@endsection
+    </div>
+  
+  </section>
