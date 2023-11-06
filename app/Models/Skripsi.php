8de4ece046a57b33407e74dffff5caf7dd5ae4d7
CaptMedkit <vicoriza1025@gmail.com>
2023-11-06 22:45:12 +0700

    SRS-06 model

--- app/Models/Skripsi.php
+++ app/Models/Skripsi.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Skripsi extends Model
 {
     use HasFactory;
+
+    protected $table = 'skripsi';
+    public $incrementing = true;
+
+    protected $fillable = [
+        'status_skripsi',
+        'nilai',
+        'tanggal_lulus_sidang',
+        'lama_studi',
+        'status',
+        'file',
+        'mahasiswa_id',
+    ];
 }
