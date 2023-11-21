f2007cc41a9d59dca8ee2b6663035698b0155104
CaptMedkit <vicoriza1025@gmail.com>
2023-11-22 01:47:11 +0700

    comment

--- app/Imports/ImportMahasiswa.php
+++ app/Imports/ImportMahasiswa.php
@@ -1,28 +1,28 @@
-<?php
+        <?php
 
-namespace App\Imports;
+        namespace App\Imports;
 
-use App\Models\User;
-use App\Models\Mahasiswa;
-use Illuminate\Support\Collection;
-use Maatwebsite\Excel\Concerns\ToModel;
-use Maatwebsite\Excel\Concerns\ToCollection;
-use Maatwebsite\Excel\Concerns\WithHeadingRow;
+        use App\Models\User;
+        use App\Models\Mahasiswa;
+        use Illuminate\Support\Collection;
+        use Maatwebsite\Excel\Concerns\ToModel;
+        use Maatwebsite\Excel\Concerns\ToCollection;
+        use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class UserMahasiswaImport implements ToModel
-{
-    public function model(array $row)
-    {
-        $mahasiswa = new Mahasiswa([
-            'nama' => $row[2], 
-        ]);
-        $mahasiswa->save();
+        class UserMahasiswaImport implements ToModel
+        {
+            public function model(array $row)
+            {
+                // $mahasiswa = new Mahasiswa([
+                //     'nama' => $row[2], 
+                // ]);
+                // $mahasiswa->save();
 
-        $user = new User([
-            'email' => $row[1], 
-            'password' => bcrypt($row[2]), 
-            'role_id' => 4,
-        ]);
-        $user->save();
-    }
-}
+                // $user = new User([
+                //     'email' => $row[1], 
+                //     'password' => bcrypt($row[2]), 
+                //     'role_id' => 4,
+                // ]);
+                // $user->save();
+            }
+        }
